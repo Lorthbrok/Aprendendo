973a382d58a9195f9216bda0bc4747d7c84758f9
Lorthbrok <gustavominephb@gmail.com>
2022-12-16 18:58:59 -0300

    Atualizando

---
+++ “PROG1 – AVAL1/guia.py
@@ -0,0 +1,32 @@
+'''
+Para Criar Instancia da classe Individual use os seguintes argumentos
+
+1 : Escolha um genotipo da lista ('Ai','AA','Bi','BB','ii')
+
+2 : Digite um nome (Opcional)
+
+Atributo 'Individual.tabela_geral' para ver os dados de todas as instâncias da classe Individual
+
+Funcao ler_genotipo() para ler o genotipo
+
+Funcao ler_tipo_sangineo() para ler tipo sanguineo
+
+Funcao ler_nome() para ler o nome
+
+Funcao ler_aglutininas() para ler as aglutininas
+
+Fucao  ler_aglutinogeneos() para ler os aglutinogeneos
+
+Funcao doar_sangue(outra instância de Individual) para saber se a instância que chamou o método pode doar para a que foi passada como parâmetro
+
+Funcao receber_sangue(outra instância de Individual) para saber se a instância que chamou o método pode receber sangue de quem foi passada como parâmetro
+
+Funcao combinar_genotipo(outra instância de Individual) combina os possiveis genotipos do cruzamento entre duas instancia
+
+Funcao combinar_sangue(outra instância de Individual) combina os possiveis tipos sanguineos do cruzamento entre duas instancia
+
+Exemplo :  pessoa_1 = Individual('Ai')
+           pessoa_2 = Individual('ii)
+           pessoa_1.doar_sangue(pessoa_2)
+
+'''
--- “PROG1 – AVAL1/main.py
+++ “PROG1 – AVAL1/main.py
@@ -5,8 +5,10 @@ class Individual () :
     
     lista_genotipos = ('AA', 'Ai', 'BB', 'Bi', 'AB', 'ii')
 
+    #Contador de  instâncias
     numero_individuos = 0
 
+    #Tabela com dados de todas as intâncias da classe
     tabela_geral = pd.DataFrame(columns=['NOME','GENOTIPO','TIPO_SANGUINEO','AGLUTINOGENEOS','AGLUTININAS'])
 
     copia = []
@@ -30,6 +32,7 @@ class Individual () :
         self.__aglutinogeneos = self.__definir_aglutinogeneos()
         self.__aglutininas = self.__definir_aglutininas()
 
+        #Usando Pandas para criar uma tabela para exibir dados
         colunas = ['NOME','GENOTIPO','TIPO_SANGUINEO','AGLUTINOGENEOS','AGLUTININAS']
         dados = [[self.__nome, self.__genotipo, self.__tipo_sanguineo, self.__aglutinogeneos, self.__aglutininas]]
         self.__tabela= tabela = pd.DataFrame(data=dados,columns=colunas)
@@ -37,13 +40,15 @@ class Individual () :
         Individual.tabela_geral.loc[Individual.numero_individuos] = [self.__nome, self.__genotipo, self.__tipo_sanguineo, self.__aglutinogeneos, self.__aglutininas]
         Individual.numero_individuos = Individual.numero_individuos + 1
 
-
+    
     def __str__ (self) :
         
         print(self.__tabela)
         
         return ' '
- 
+    
+
+    #Funcao para definir o tipo sanguineo
     def __definir_tipo_sanguineo (self) :
 
         if self.__genotipo == 'AA' :
@@ -66,20 +71,24 @@ class Individual () :
 
         return self.__tipo_sanguineo
 
+    #Funcao que gera o nome automaticamete quando nao for definido no construtor
     def __validar_genotipo (self) :
 
         if self.__genotipo not in Individual.lista_genotipos :
 
-            raise 'Valor de genotipo inválido'
+            raise 'Valor de genotipo inválido'                   
     
+
+    #Funcao que validaçao do valor de entrada do genotipo
     def __gerar_nome (self) :
 
-        num_ind = Individual.numero_individuos + 1
+        num_ind = Individual.numero_individuos + 1             
         
         nome = 'Indiv-N' + str(num_ind)
         
         return nome
 
+    #Funcao que definir os aglutinogeneos
     def __definir_aglutinogeneos (self) :
 
         if self.__tipo_sanguineo == 'A' :
@@ -95,7 +104,8 @@ class Individual () :
             self.__aglutinogeneos = 'Não possui'
         
         return self.__aglutinogeneos
-    
+
+    #Funcao que definir as aglutininas
     def __definir_aglutininas (self) :
         
         if self.__tipo_sanguineo == 'A' :
@@ -112,6 +122,7 @@ class Individual () :
         
         return self.__aglutininas
 
+    #Funçoes para ler os atributos privados
     def ler_genotipo (self) :
 
         return print(self.__genotipo)
@@ -138,6 +149,7 @@ class Individual () :
         else :
             return print(f'Aglutotinas do tipo {self.__aglutininas}')
 
+    #Funcao que analisa a possibilidade de doação de sangue
     def doar_sangue (self, quem_recebe) :
         
         if self.__tipo_sanguineo == 'A' and quem_recebe.__tipo_sanguineo == 'A' :
@@ -161,6 +173,7 @@ class Individual () :
         else :
             return print(f'O/A {self.__nome} TIPO_SANGUINEO({self.__tipo_sanguineo}) NÂO pode doar sangue para o/a {quem_recebe.__nome} TIPO_SANGUINEO({quem_recebe.__tipo_sanguineo})')
     
+    #Funcao que analisa a possibilidade da recepçao de sangue
     def receber_sangue (self, quem_doa) :
         if self.__tipo_sanguineo == 'A' and quem_doa.__tipo_sanguineo == 'A' or 'O':
             return print(f'O/A {self.__nome} TIPO_SANGUINEO({self.__tipo_sanguineo}) PODE receber sangue do o/a {quem_doa.__nome} TIPO_SANGUINEO({quem_doa.__tipo_sanguineo})')
@@ -177,6 +190,7 @@ class Individual () :
         else:
             return print(f'O/A {self.__nome} TIPO_SANGUINEO({self.__tipo_sanguineo}) NÂO pode receber sangue do o/a {quem_doa.__nome} TIPO_SANGUINEO({quem_doa.__tipo_sanguineo})')
 
+    #Funcao que analisa as possiveis combinaçoes de genotipos entre dois objetos
     def combinar_genotipo (self, outro_individuo, gambiarra=True) :
         
         g1 = []
@@ -209,7 +223,8 @@ class Individual () :
             return print(f'Os possíveis genótipos resultantes do cruzamento entre {self.__nome} e {outro_individuo.__nome} são {combinacao}')
         else :
             pass
-    
+
+    #Funcao que analisa as possiveis combinações de tipos sanguineos entre dois objetos
     def combinar_sangue (self, outro_individuo) :
         
         self.combinar_genotipo(outro_individuo, gambiarra=False)
